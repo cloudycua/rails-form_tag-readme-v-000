94fb53216f51ee8a16c4e7d73f72a80cd539c7d6
Christine Cua <cloudycua@gmail.com>
2018-08-21 00:51:37 +0000

    automatically backed up by learn

--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -18,6 +18,20 @@ describe 'new post' do
     fill_in 'post_title', with: 'My post title'
     fill_in 'post_description', with: 'My post description'
 
+    click_on 'Submit Post'
+
+    expect(page.current_path).to eq(posts_path)
+    expect(page).to have_content('My post title')
+    expect(page).to have_content('My post description')
+  end
+
+  it "displays a new post form that redirects to the index page, which then contains the submitted post's title and description" do
+    visit new_post_path
+    fill_in 'post_title', with: 'My post title'
+    fill_in 'post_description', with: 'My post description'
+
+    
+
     expect(page.current_path).to eq(posts_path)
     expect(page).to have_content('My post title')
     expect(page).to have_content('My post description')
